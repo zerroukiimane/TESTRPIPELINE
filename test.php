620ca03ab730a7125e06d83a739d562108661da8
ZERROUKI Imane <119601684+zerroukiimane@users.noreply.github.com>
2024-07-21 14:01:42 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
  "<? php 
-if (strpos(file_get_contents(index.php'),'hello,Jenkins!') !==false) {
+if (strpos(file_get_contents(index.php'),'Hello,Jenkins!') !==false) {
 echo "TestPassed";
 } else { 
 echo "Test Failed";
